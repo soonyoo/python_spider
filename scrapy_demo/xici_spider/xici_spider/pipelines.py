d60622fdf7afef869bf5d1af284f156661259482
xuwenyuan <64174469@qq.com>
2019-05-08 23:36:45 +0800

    重写西刺xici.py的start_requests方法

--- scrapy_demo/xici_spider/xici_spider/pipelines.py
+++ scrapy_demo/xici_spider/xici_spider/pipelines.py
@@ -10,15 +10,20 @@ from scrapy.utils.project import get_project_settings
 
 class XiciSpiderPipeline(object):
     def __init__(self):
+        # 从settings.py中读取键值
         settings = get_project_settings()
-        DBKWARGS = settings.get("DBKWARGS")
-        self.conn = pymysql.connect(**DBKWARGS)
+        dbkwargs = settings.get("DBKWARGS")
+        self.conn = pymysql.connect(**dbkwargs)
         self.cursor = self.conn.cursor()
         self._sql = None
 
     def process_item(self, item, spider):
-        self.cursor.execute(self.sql, (item['ip'], item['port'], item['position'], item['type'], item['speed'], item['last_check_time']))
-        self.conn.commit()
+        try:
+            self.cursor.execute(self.sql, (item['ip'], item['port'], item['position'], item['type'], item['speed'], item['last_check_time']))
+            self.conn.commit()
+        except Exception as ex:
+            print('Insert Error' + str(ex))
+            self.conn.rollback()
         return item
 
     @property
--- scrapy_demo/xici_spider/xici_spider/spiders/xici.py
+++ scrapy_demo/xici_spider/xici_spider/spiders/xici.py
@@ -10,11 +10,16 @@ class XiciSpider(scrapy.Spider):
     allowed_domains = ['xicidaili.com']
     start_urls = ['https://www.xicidaili.com/nn/1']
 
+    def start_requests(self):
+        reqs = []
+        for i in range(11):
+            req = scrapy.Request('https://www.xicidaili.com/nn/{}'.format(i))
+            reqs.append(req)
+        return reqs
+
     def parse(self, response):
-        # print('进来了。。。。')
+        # 跳过第一行，字段行
         trs = response.xpath("//table[@id='ip_list']/tr")[1:]
-        # print(type(trs))
-        # print(len(trs))
         for tr in trs:
             tds = tr.xpath("./td")
             ip = tds[1].xpath("./text()").get()
@@ -22,6 +27,7 @@ class XiciSpider(scrapy.Spider):
             position = tds[3].xpath("./a/text()").get()
             type = tds[5].xpath("./text()").get()
             speed = tds[6].xpath("./div/@title").get()
+            # 使用正则表达式，0.803秒 ==> 0.803 (去掉中文内容)
             speed = re.findall(r"\d+\.\d+", speed)[0]
             last_check_time = tds[-1].xpath("./text()").get()
             item = XiciSpiderItem(ip=ip, port=port, position=position, type=type, speed=speed, last_check_time=last_check_time)
